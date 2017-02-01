6922c1b9a71464d0d7876bfd404877f7e03f1b1f
ramabose <ramabose@hotmail.com>
2017-02-01 18:19:33 +0530

    Update test.php

--- test.php
+++ test.php
@@ -15,7 +15,7 @@ if($resCnt>0)
 			$ExeInsQuery=mysql_query($UPQuery);
 	}
 }*/
-/*$selQuery="SELECT * FROM `contacts` where FIND_IN_SET('2',`ContactType`) and `TenantType`!='' and `TenantType`!='0'";
+$selQuery="SELECT * FROM `contacts` where FIND_IN_SET('2',`ContactType`) and `TenantType`!='' and `TenantType`!='0'";
 $results=mysql_query($selQuery);
 $resCnt=mysql_num_rows($results);
 if($resCnt>0)	
@@ -27,6 +27,6 @@ if($resCnt>0)
 			$ExeInsQuery=mysql_query($UPQuery);
 	}
 	
-}*/
+}
 
 ?>
